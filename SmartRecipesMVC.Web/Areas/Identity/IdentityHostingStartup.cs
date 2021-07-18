3b25d6303932543588907521d41eca4b13ad79d9
Michał Czaja <35144@studentwsei.pl>
2021-07-18 17:26:05 +0200

    Generating user management views

---
+++ SmartRecipesMVC.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SmartRecipesMVC.Infrastructure;
+
+[assembly: HostingStartup(typeof(SmartRecipesMVC.Web.Areas.Identity.IdentityHostingStartup))]
+namespace SmartRecipesMVC.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

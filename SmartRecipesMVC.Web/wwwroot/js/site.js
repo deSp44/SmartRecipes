257f86fe8ec2f9aae49c67fe769c3f4990034265
Michał Czaja <35144@studentwsei.pl>
2021-09-16 13:00:13 +0200

    Add button to smooth scroll to top

--- SmartRecipesMVC.Web/wwwroot/js/site.js
+++ SmartRecipesMVC.Web/wwwroot/js/site.js
@@ -1,3 +1,30 @@
-﻿$(document).ready(function () {
+﻿// DROPDOWN TOGGLE
+$(document).ready(function () {
     $(".dropdown-toggle").dropdown();
 });
+
+
+// SCROLL TOP BUTTON
+let mybutton = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+    ) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
